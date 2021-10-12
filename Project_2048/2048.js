b5492d09a070fd50afde7f5855c7dbf4ce61e969
ImrePeter2001 <78748876+ImrePeter2001@users.noreply.github.com>
2021-10-12 13:52:33 +0200

    Add files via upload

---
+++ Project_2048/2048.js
@@ -0,0 +1,6 @@
+function start(){
+    var Starting_S = document.getElementById("Starting_S");
+    var Game = document.getElementById("Game");
+    Starting_S.style.display="none";
+    Game.style.display="block";   
+}
